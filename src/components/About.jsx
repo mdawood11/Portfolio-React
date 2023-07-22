ef8ce2c9ca41ca0d3037ce3182cb936b196c683f
mohamad ghaznawy <mdawoodghaznawy@yahoo.com>
2023-07-21 19:54:54 -0700

    Modified About.jsx file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>Hi...</p>
+            <p>Hi....</p>
           </div>
           <div>
             <p>
