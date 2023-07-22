5554c242d18aa1e0a6080a81a5a744bfc3e45168
mohamad ghaznawy <mdawoodghaznawy@yahoo.com>
2023-07-21 19:11:38 -0700

    Copleted navbar.jsx & Modified home.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,186 @@
-import React from "react";
+// import React from "react";
+// import { FaBars, FaTimes } from "react-icons/fa";
+// import logo from "../assets/logo.png";
 
-function Navbar() {
+// function Navbar() {
+//   return (
+//     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+//       <div>
+//         <img src={logo} alt="logo" style={{ width: "150px" }} />
+//       </div>
+
+//       {/* menu */}
+//       <div>
+//         <ul className="flex">
+//           <li>Home</li>
+//           <li>About</li>
+//           <li>Skills</li>
+//           <li>Works</li>
+//           <li>Contact</li>
+//         </ul>
+//       </div>
+
+//       {/* humburger */}
+//       <div className="hidden">
+//         <FaBars />
+//       </div>
+
+//       {/* Mobile menu */}
+
+//       <ul className="hidden">
+//         <li>Home</li>
+//         <li>About</li>
+//         <li>Skills</li>
+//         <li>Works</li>
+//         <li>Contact</li>
+//       </ul>
+
+//       {/* Social Icons */}
+//       <div></div>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+import React, { useState } from "react";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+} from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import Logo from "../assets/logo.png";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+    <div className="fixed w-full h-[90px] flex justify-between lg:px-[440px] md:px-8 items-center bg-[#0c1e38] text-gray-300 ">
+      <div className="">
+        <img
+          src={Logo}
+          alt="Logo Image"
+          style={{ width: "50px", height: "70px" }}
+        />
+      </div>
+
+      {/* menu */}
+      <ul className="hidden md:flex">
+        <li className="hover:text-logo-color ease-in duration-300 font-semibold">
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="hover:text-logo-color ease-in duration-300 font-semibold">
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="hover:text-logo-color ease-in duration-300 font-semibold">
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="hover:text-logo-color ease-in duration-300 font-semibold">
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="hover:text-logo-color ease-in duration-300 font-semibold">
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* hamburguer */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "md:hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0A66C2]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2D333B]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EA4335]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              E-mail <MdEmail size={30} />
+            </a>
+          </li>
+        </ul>
+        <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#077AE9]">
+          <a
+            className="flex justify-between items-center w-full text-gray-300"
+            href="/"
+          >
+            Facebook <FaFacebook size={30} />
+          </a>
+        </li>
+      </div>
+    </div>
   );
-}
+};
 
 export default Navbar;
